4f0b88b4a79eff714ca572cc197c80d770cbed39
hippieZhou <hippiezhou@outlook.com>
2019-12-14 11:41:48 +0800

    change dnc 2.2 to 3.1

---
+++ src/Supermarket.API/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Supermarket.API.Extensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(cfg =>
+            {
+                cfg.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Supermarket API",
+                    Version = "v1.1",
+                    Description = "Simple RESTful API built with ASP.NET Core 3.1 to show how to create RESTful services using a decoupled, maintainable architecture.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Evandro Gayer Gomes",
+                        Url = new Uri("https://github.com/evgomes")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT",
+                    },
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                cfg.IncludeXmlComments(xmlPath);
+            });
+            return services;
+        }
+
+        public static IApplicationBuilder UseCustomSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger().UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Supermarket API");
+                options.DocumentTitle = "Calendar API";
+                options.RoutePrefix = string.Empty;
+            });
+            return app;
+        }
+    }
+}
--- src/Supermarket.API/Persistence/Contexts/AppDbContext.cs
+++ src/Supermarket.API/Persistence/Contexts/AppDbContext.cs
@@ -1,4 +1,3 @@
-using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.InMemory.ValueGeneration.Internal;
 using Supermarket.API.Domain.Models;
@@ -18,7 +17,7 @@ namespace Supermarket.API.Persistence.Contexts
             
             builder.Entity<Category>().ToTable("Categories");
             builder.Entity<Category>().HasKey(p => p.Id);
-            builder.Entity<Category>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd().HasValueGenerator<InMemoryIntegerValueGenerator<int>>();
+            builder.Entity<Category>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();//.HasValueGenerator<InMemoryIntegerValueGenerator<int>>();
             builder.Entity<Category>().Property(p => p.Name).IsRequired().HasMaxLength(30);
             builder.Entity<Category>().HasMany(p => p.Products).WithOne(p => p.Category).HasForeignKey(p => p.CategoryId);
 
