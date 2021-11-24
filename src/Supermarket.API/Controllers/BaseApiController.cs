360bd6c8d804a0b863d34d22fc3fe2fc5c73f25e
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-11-24 23:19:41 +0200

    Refactored Controllers by adding BaseApiContorller

---
+++ src/Supermarket.API/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Supermarket.API.Controllers
+{
+    [Route("/api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
